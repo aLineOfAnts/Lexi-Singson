a43e134218687fb0b279621ea910f72e4ceec87b
ShelbyOnTheSeashells <clqxwnfish@gmail.com>
2025-07-05 12:11:01 +0800

    about me page desktop progress

--- app/about-me/Languages.tsx
+++ app/about-me/Languages.tsx
@@ -3,17 +3,32 @@ import "./about-me.css"
 export default function Languages() {
     return (
         <section className="skills-container">
+            
             <div className="skills-wrapper">
-                <img src="ui/About-Me/Languages.png"></img>
-                <div className="skills-list">
-                    <div className="skills-list-slot"></div>
-                    <div className="skills-list-slot"></div>
-                    <div className="skills-list-slot"></div>
-                    <div className="skills-list-slot"></div>
-                    <div className="skills-list-slot"></div>
-                    <div className="skills-list-slot"></div>
+                <img src="ui/About-Me/Languages.png" className="skills-label"></img>
+                <div className="skills-list-wrapper">
+                    <div className="skills-list">
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                        <div className="skills-list-slot"></div>
+                    </div>
+                    <div className="skills-list-page-label">
+                        <p>1 of 2</p>
+                        <button><img src="ui/About-Me/Arrow Right.svg"></img></button>
+                    </div>
                 </div>
+                
             </div>
+            
+                
+            
         </section>
     )
 }
--- app/about-me/Profile.tsx
+++ app/about-me/Profile.tsx
@@ -11,9 +11,12 @@ export default function Profile() {
                 </div>
 
                 <div className="info">
-                    <img src="ui/About-Me/Greeting.Svg"></img>
+                    <div className="about-me-profile-greeting">
+                         <img src="ui/About-Me/Greeting.Svg" className="about-me-profile-greeting-label"></img>
+                        <p>My name is Lexi, I’m an upcoming Computer</p> 
+                    </div>
+                   
                     <p>
-                        My name is Lexi, I’m an upcoming Computer <br></br>
                         Science student specializing in Software Engineering! I like to make things, like draw and code. I also take an interest in tabletop RPGs. I like to walk outdoors and eat sweets. and etc etc blah blah omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
 
     omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptatesdebitis.
