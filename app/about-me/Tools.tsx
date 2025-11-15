508e13577ef23d458cf5b3005eede29fe215544d
ShelbyOnTheSeashells <clqxwnfish@gmail.com>
2025-11-16 00:15:31 +0800

    tools

--- app/about-me/Tools.tsx
+++ app/about-me/Tools.tsx
@@ -13,10 +13,9 @@ const languages = [
     {"name" : "C++", "url" : ""},
     {"name" : "C++", "url" : ""}
 ]
-export default function Tools() {
-    return (
-        <section className="skills-container">
-            <div className="skills-wrapper">
+/*
+
+<div className="skills-wrapper">
                 <img src="ui/About-Me/Tools.png" className="skills-label skills-tool-label"></img>
                 <div className="skills-label-wrapper"></div>
                 <div className="skills-list-wrapper">
@@ -29,6 +28,25 @@ export default function Tools() {
                     </div>
                 </div>
                 
+            </div>
+*/
+
+export default function Tools() {
+    return (
+        <section className="skills-container">
+            <div className="skills-content-wrapper">
+                <div className="skills-label-wrapper">
+                    <img src="ui/About-Me/Tools.png" className="skills-label skills-tool-label"></img>
+                </div>
+                <div className="skills-list-wrapper2">
+                    <div className="skills-list">
+                        {languages.map((skill, index) => { return <Skill name={skill.name} url={skill.url} key={index} light={true}></Skill>} )}
+                    </div>
+                    <div className="skills-list-page-label hidden">
+                        <p>1 of 2</p>
+                        <button><img src="ui/About-Me/Arrow Right.svg"></img></button>
+                    </div>
+                </div>
             </div>
             
                 
