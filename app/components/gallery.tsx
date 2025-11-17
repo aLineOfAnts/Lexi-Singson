059a3647fdc1e78f44eab84f06a4d91a9d1a6fc2
ShelbyOnTheSeashells <clqxwnfish@gmail.com>
2025-11-17 11:26:27 +0800

    refactor

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -1,5 +1,92 @@
-export default function Gallery() {
-    return (<>
+'use client'
+import { ReactNode } from "react";
+import { useState } from "react";
+import Project from "./projects/project/project";
+
+
+
+interface GalleryProps {
+    list : any[]
+    galleryType : string
+}
+export default function Gallery({list, galleryType} : GalleryProps) {
+    const [index, setIndex] = useState(0);
+    const [page, setPage] = useState(1);
+    const pages = Math.round(list.length / 5)
     
-    </>);
+
+    const CheckPage = (newIndex : number) => {
+        const leftArrow = document.getElementById("projects-left-arrow")
+            if (leftArrow) {
+            if (newIndex <= 0) {
+                leftArrow.classList.remove("shown");
+                leftArrow.classList.add("hidden");
+            } else {
+                leftArrow.classList.add("shown");
+                leftArrow.classList.remove("hidden");
+            }
+        }
+        
+
+        const rightArrow = document.getElementById("projects-right-arrow")
+        if (!rightArrow) { return }
+        if (newIndex + 6 >= list.length || newIndex >= list.length) {
+            rightArrow.classList.remove("shown");
+            rightArrow.classList.add("hidden");
+        } else {
+            rightArrow.classList.remove("hidden");
+            rightArrow.classList.add("shown");
+        }
+            
+        
+    }
+
+    const DisplayGallery = () => {
+        if (galleryType == "projects") {
+            console.log("projects");
+            return <>{list.slice(index, index+6).map((item, index) => { return <Project key={index} title={item.name} description={item.description} image={item.image}></Project>})}</>
+            
+        } else if (galleryType == "artworks") {
+            return <></>
+        }
+        return <></>
+    }
+        
+    
+    return (
+            <div className="projects-content-wrapper">
+                <div className="projects-list">
+                    {DisplayGallery()}
+                    
+                </div>
+                <div className="projects-list-page-indicator">
+                    <div>
+                            <input type="image" src="/ui/Projects/ui/Arrow left.svg" className="hidden" id="projects-left-arrow" 
+                        onClick={() => {
+                            if (index <= 0) { return; }
+                            const newIndex = index - 6
+                            setIndex(newIndex)
+                            setPage(page - 1)
+                            CheckPage(newIndex)
+                        }}></input>
+                    </div>
+                    <div>
+                        <p>{page} of {pages}</p>
+                    </div>
+                    <div>
+                        <input type="image" src="/ui/Projects/ui/Arrow right.svg"  id="projects-right-arrow" onClick={() => {
+                        const newIndex = index + 6
+                        if (newIndex > list.length) { return }
+                        if (newIndex >= list.length) { return }
+                        setIndex(newIndex);
+                         setPage(page + 1)
+                        CheckPage(newIndex)
+                    }}></input>
+                    </div>
+                    
+                    
+
+                </div>
+            </div>
+    )
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,7 @@
 import "./projects.css"
 import Project from "../components/projects/project/project"
 import { ReactNode, useState } from "react"
+import Gallery from "../components/gallery"
 
 
 const projects = [
@@ -59,73 +60,10 @@ interface GalleryProps {
     listItem : ReactNode
 }
 export default function Projects() {
-    const [index, setIndex] = useState(0);
-    const [page, setPage] = useState(1);
-    const pages = Math.round(projects.length / 5)
-
-    const CheckPage = (newIndex : number) => {
-        const leftArrow = document.getElementById("projects-left-arrow")
-            if (leftArrow) {
-            if (newIndex <= 0) {
-                leftArrow.classList.remove("shown");
-                leftArrow.classList.add("hidden");
-            } else {
-                leftArrow.classList.add("shown");
-                leftArrow.classList.remove("hidden");
-            }
-        }
-        
-
-        const rightArrow = document.getElementById("projects-right-arrow")
-        if (!rightArrow) { return }
-        if (newIndex + 6 >= projects.length || newIndex >= projects.length) {
-            rightArrow.classList.remove("shown");
-            rightArrow.classList.add("hidden");
-        } else {
-            rightArrow.classList.remove("hidden");
-            rightArrow.classList.add("shown");
-        }
-            
-        
-    }
     return (
         <section className="projects-section">
             <h1>Projects</h1>
-            <div className="projects-content-wrapper">
-                <div className="projects-list">
-                    {projects.slice(index, index+6).map((project, index) => { return <Project title={project.name} description={project.description} image={project.image}></Project>})}
-                </div>
-                <div className="projects-list-page-indicator">
-                    <div>
-                            <input type="image" src="/ui/Projects/ui/Arrow left.svg" className="hidden" id="projects-left-arrow" 
-                        onClick={() => {
-                            if (index <= 0) { return; }
-                            const newIndex = index - 6
-                            setIndex(newIndex)
-                            setPage(page - 1)
-                            CheckPage(newIndex)
-                        }}></input>
-                    </div>
-                    <div>
-                        <p>{page} of {pages}</p>
-                    </div>
-                    <div>
-                        <input type="image" src="/ui/Projects/ui/Arrow right.svg"  id="projects-right-arrow" onClick={() => {
-                        const newIndex = index + 6
-                        if (newIndex > projects.length) { return }
-                        if (newIndex >= projects.length) { return }
-                        setIndex(newIndex);
-                         setPage(page + 1)
-                        CheckPage(newIndex)
-                    }}></input>
-                    </div>
-                    
-                    
-                    
-
-                </div>
-            </div>
-
+            <Gallery list={projects} galleryType="projects"></Gallery>
         </section>
     )
 }
