d9c8295c5792c11b131756f4ffeab6d05da0c8ff
ShelbyOnTheSeashells <clqxwnfish@gmail.com>
2025-11-21 01:23:47 +0800

    Experience

--- app/about-me/Experience.tsx
+++ app/about-me/Experience.tsx
@@ -5,25 +5,42 @@ import ExperienceProfile from "../components/about-me/work-experience/experience
 const experiences = [
     { 
         "position" : "Frontend Systems Programmer",
-        "date" : "March 16 - Present",
+        "date" : "June 2025 - Present",
         "company" : "Onecore Consultancy Inc.",
         "description" : ["Implemented pages for the HRIS system", "Created the system foundation", "Responsible for the visibility and interactivity of the site."]
     },
 
     { 
         "position" : "Part-Time Mobile Developer",
-        "date" : "March 16 - Present",
+        "date" : "March 2025 - Present",
         "company" : "Lightbear Software",
         "description" : ["Responsible for fixing bugs and implementing features on the educational apps.", "Worked on Sentence Reading Magic 2", "Worked on Reading Boost Blending"]
     },
 
     { 
         "position" : "Web Development Intern",
-        "date" : "March 16 - Present",
+        "date" : "April - May 2025",
         "company" : "Property Company of Friends, Inc.",
         "description" : ["Responsible for designing UI pages of Onboarding Portal.", "Conceptualized user experience."]
     }
 ]
+
+const education = [
+    {
+        "position" : "IMGG Treasurer",
+        "date" : "March 2024 - June 2025",
+        "company" : "iACADEMY",
+        "description" : ["Solely led two lectures to an audience of around 30 students about programming twice.", "Involved in planning events for the year.", "Involved in marketing the organization and events."]
+    },
+
+    {
+        "position" : "Wordpress Developer",
+        "date" : "March - May 2023",
+        "company" : "Temple Hills International Inc.",
+        "description" : ["In charge of managing the newspaper website"]
+    }
+]
+
 export default function Experience() {
     return (
         <section className="experience-section">
