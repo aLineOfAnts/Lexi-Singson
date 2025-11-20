bb15428738317940ba12706d78e8efbd215a43f4
ShelbyOnTheSeashells <clqxwnfish@gmail.com>
2025-11-20 21:33:05 +0800

    languages

--- app/about-me/Languages.tsx
+++ app/about-me/Languages.tsx
@@ -3,15 +3,16 @@ import Skill from "../components/about-me/skill"
 const languages = [
 
     {"name" : "C++", "url" : "ui/About-Me/Languages/C++.png"},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined},
-    {"name" : "C++", "url" : undefined}
+    {"name" : "C#", "url" : "ui/About-Me/Languages/CSharp.png"},
+    {"name" : "Java", "url" : "ui/About-Me/Languages/Java.png"},
+    {"name" : "Python", "url" : "ui/About-Me/Languages/Python.png"},
+    {"name" : "HTML", "url" : "ui/About-Me/Languages/HTML.png"},
+    {"name" : "CSS", "url" : "ui/About-Me/Languages/CSS.png"},
+    {"name" : "Javascript", "url" : "ui/About-Me/Languages/Javascript.png"},
+    {"name" : "Javascript", "url" : "ui/About-Me/Languages/Typescript.png"},
+    {"name" : "PHP", "url" : "ui/About-Me/Languages/PHP.png"},
+    {"name" : "Swift", "url" : "ui/About-Me/Languages/Swift.png"},
+    
 ]
 
 /*
@@ -30,7 +31,7 @@ export default function Languages() {
                     <div className="skills-list">
                         {languages.map((skill, index) => { return <Skill name={skill.name} skillUrl={skill.url} key={index} light={false}></Skill>} )}
                     </div>
-                    <div className="skills-list-page-label">
+                    <div className="skills-list-page-label hidden">
                         <p>1 of 2</p>
                         <button><img src="ui/About-Me/Arrow Right.svg"></img></button>
                     </div>
